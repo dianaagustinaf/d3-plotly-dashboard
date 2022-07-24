6dfb7cd4255e699629ca3f7d968825763d3206fe
dianaagustinaf <dianaagustinaf@gmail.com>
2022-07-24 23:42:22 +0100

    bar chart - not finished

--- static/js/app.js
+++ static/js/app.js
@@ -32,14 +32,87 @@
  */
 
 
-url="https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+function charts(individual) {
 
-d3.json(url).then((data) => {
 
-    const ids = data.names;
-    const metadata = data.metadata;
-    const samples = data.samples;
-    //console.log(ids);
+    // LOAD DATA
 
-})
+    url="https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
+    let ids;
+    let metadata;
+    let samples;
+
+    d3.json(url).then((data) => {
+
+        ids = data.names;
+        metadata = data.metadata;
+        samples = data.samples;
+        console.log(ids);
+        // console.log(metadata);
+        // console.log(samples);
+
+    
+
+
+    // BAR CHART
+    /**
+    Use sample_values as the values for the bar chart.
+    Use otu_ids as the labels for the bar chart.
+    Use otu_labels as the hovertext for the chart.
+    */
+
+    // let individual = 1601
+    let top = 10;
+    let i = 0;
+    let xdata = [];
+    let ydata = [];
+    let otu_hover = [];
+
+
+
+    let indiv_data = samples.filter(ind => ind.id == individual);
+    //samples[individual];
+    console.log(indiv_data);
+
+    while (i<top) {
+        xdata[i] = indiv_data[0].sample_values[i];
+        ydata[i] = indiv_data[0].otu_ids[i];
+        otu_hover[i] = indiv_data[0].otu_labels[i];
+        i++;
+    }
+
+    var trace1 = {
+        x: xdata,
+        y: ydata,
+        text: otu_hover,
+        name: 'SF Zoo',
+        orientation: 'h',
+        marker: {
+        color: 'rgba(55,128,191,0.6)',
+        width: 1
+        },
+        type: 'bar'
+    };
+    
+    var data = [trace1];
+    
+    var layout = {
+        title: 'Top 10 OTUs found in selected individual',
+        barmode: 'stack'
+    };
+    
+    Plotly.newPlot('bar', data, layout);
+
+
+
+    });
+};
+
+charts(1601);
+
+
+// let dropd = d3.select("#selDataset");
+// ids.forEach(function(id) {
+//     dropd.append('option').text(id);
+// });
